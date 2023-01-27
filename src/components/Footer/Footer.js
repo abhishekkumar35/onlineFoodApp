09656c779d30de86ad8b97e759cb04c467852a34
Abhishek Kumar <abhishek.kumar.cse19@gmail.com>
2023-01-27 14:47:45 +0530

    changed style of footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,22 @@
 const Footer = () => {
+  const styles = {
+    footer__main: {
+      backgroundColor: "yellow",
+      width: "100%",
+      padding: "4px",
+      border: "1px solid gray",
+      borderRadius: "5px",
+    },
+    footer__card: {
+      display: "flex",
+      justifyContent: "center",
+    },
+  };
   return (
-    <div>
-      <p>Footer</p>
+    <div style={styles.footer__card}>
+      <div>
+        <marquee style={styles.footer__main}>Created By Abhishek Kumar</marquee>
+      </div>
     </div>
   );
 };
